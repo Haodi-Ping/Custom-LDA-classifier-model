7ca617208a445a84cbeaf1723f815cf02dfd5a28
karven <33412671+Haodi-Ping@users.noreply.github.com>
2022-08-28 18:24:46 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+
+import bentoml
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# import custom model class
+from lda import LDA
+
+def main(request):
+    fun_st = time.time() * 1000
+    # Load the dataset
+    data = datasets.load_iris()
+    X = data.data
+    y = data.target
+
+    # Three -> two classes
+    X = X[y != 2]
+    y = y[y != 2]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+    # Fit and predict using LDA
+    lda = LDA()
+    lda.fit(X_train, y_train)
+    y_pred = lda.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print ("Accuracy:", accuracy)
+
+    # Save model with BentoML
+    saved_model = bentoml.picklable_model.save_model(
+        'iris_clf_lda',
+        lda,
+        signatures={"predict": {"batchable": True}},
+    )
+    print(f"Model saved: {saved_model}")
+
+    
+# main("")
+# if __name__ == "__main__":
+#     main()
